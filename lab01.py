16146932042ef22b01bc9e583bcb7b133d478b51
unknown <lajja.tharwala@ontariotechu.net>
2023-09-13 10:49:12 -0400

    Lab 01 completed code

--- lab01.py
+++ lab01.py
@@ -2,11 +2,17 @@ def main():
     cost_per_item = 19.99
     quantity = 5 
 
-    # YOUR CODE TO CALCULATE THE TOTALS GOES HERE    
+    subtotal_cost: float = (cost_per_item)*(5) #cost per item multiplied by how many items there are
+    tax: float = (subtotal_cost)*(0.13) #tax is 13% of subtotal cost
+    total_cost: float = (subtotal_cost + tax) #adding subtotal cost and tax gived total cost
+    
+    #pytest --capture=sys
 
-    print(f'cost_per_item = ${cost_per_item:0.2f}') # a sample for you to use for the other prices
-
-    # YOUR CODE TO PRINT THE TOTALS GOES HERE
-
+    print(f'cost_per_item = ${cost_per_item:0.2f}')
+    print(f'quantity = 5')
+    print(f'subtotal_cost = ${subtotal_cost:0.2f}')
+    print(f'tax = ${tax:0.2f}') 
+    print(f'total_cost = ${total_cost:0.2f}')
+    
 if __name__ == "__main__":
     main()
